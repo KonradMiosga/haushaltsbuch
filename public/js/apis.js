f8a007e69b9d9265b344fbc5379ba6ed960ce9d6
Michael <...>
2025-01-23 23:58:09 +0100

    API Page fertig

--- public/js/apis.js
+++ public/js/apis.js
@@ -2,6 +2,9 @@ import config from './config.js';
 
 document.addEventListener('DOMContentLoaded', function () {
     chuck();
+    // IP holen, dann Wetterdaten holen, dann Graph zeichnen
+    getIP();
+    getNasa();
 });
 
 // nutzung von graphql server, dieser holt sich einen Chuck Norris Witz und lässt ihn 
@@ -65,3 +68,127 @@ function translate(text) {
     });
 }
 */
+
+// Variablen für Wetterdaten und Standortdaten
+var xValuesTemp = [];
+var yValuesTemp = [];
+var xValuesRain = [];
+var yValuesRain = [];
+var ipaddress = null;
+var city = null;
+var latitude = null;
+var longitude = null;
+
+//IP-Adresse holen
+function getIP() {
+    $.ajax({
+        url: 'http://ip-api.com/json/',
+        type: 'GET',
+        dataType: 'json',
+        success: function (data) {
+            ipaddress = data.query;
+            city = data.city;
+            latitude = data.lat;
+            longitude = data.lon;
+            console.log(data);
+            $('#idForIp').html('IP-Adresse:  ' + ipaddress);
+            $('#idForCity').html('Stadt:  ' + city);
+            wether();
+        },
+        error: function (xhr, ajaxOptions, thrownError) {
+            console.log('Error beim Laden der Standortdaten:', thrownError);
+            $('#idForIp').html('keine IP-Adresse gefunden');
+        }
+    });
+}
+
+//Wetterdaten holen
+function wether() {
+    $.ajax({
+        //url: 'https://api.open-meteo.com/v1/forecast?latitude=51.3&longitude=10.3&hourly=temperature_2m,precipitation_probability,rain',
+        url: 'https://api.open-meteo.com/v1/forecast?latitude=' + latitude
+            + '&longitude=' + longitude + '&hourly=temperature_2m,precipitation_probability,rain',
+        type: 'GET',
+        dataType: 'json',
+        success: function (data) {
+            // Nur die ersten 12 Stunden verwenden
+            for (let i = 0; i < 13; i++) {
+                xValuesTemp[i] = i*2 + ":00";
+                yValuesTemp[i] = data.hourly.temperature_2m[i*2];
+                xValuesRain[i] = i*2 + ":00";
+                yValuesRain[i] = data.hourly.rain[i*2];
+            }
+            // Graph erst zeichnen, nachdem Daten verfügbar sind
+            graph();
+        },
+        error: function (xhr, ajaxOptions, thrownError) {
+            console.log('Error beim Laden der Wetterdaten:', thrownError);
+            $('#idForWeather').html('keine Wetterdaten gefunden');
+        }
+    });
+}
+
+//Graph zeichnen
+function graph() {
+    new Chart("myChartTemp", {
+        type: "line",
+        data: {
+            labels: xValuesTemp,
+            datasets: [{
+                fill: false,
+                backgroundColor: "red",
+                borderColor: "red",
+                data: yValuesTemp,
+                label: 'Temperatur °C'
+            }]
+        },
+        options: {
+            legend: { display: true },
+            scales: {
+                yAxes: [{ ticks: { min: Math.ceil(Math.min(...yValuesTemp) - 10), max: Math.ceil(Math.max(...yValuesTemp) + 10) } }],
+            }
+        }
+    });
+    new Chart("myChartRain", {
+        type: "line",
+        data: {
+            labels: xValuesRain,
+            datasets: [{
+                fill: true,
+                backgroundColor: "blue",
+                borderColor: "blue",
+                data: yValuesRain,
+                label: 'Regen mm/h'
+            }]
+        },
+        options: {
+            legend: { display: true },
+            scales: {
+                yAxes: [{ ticks: { 
+                    min: 0, 
+                    max: Math.ceil(Math.max(...yValuesRain) + 10) 
+                }}],
+            }
+        }
+    });
+}
+
+//NASA-Daten holen
+function getNasa() {
+    $.ajax({
+        url: 'https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY',
+        type: 'GET',
+        dataType: 'json',
+        success: function (data) {
+            console.log(data);
+            $('#nasaTitle').html('Title: ' + data.title);
+            $('#nasaDate').html('Date: ' + data.date);
+            $('#nasaExplanation').html(data.explanation);
+            $('#nasaImage').attr('src', data.url);
+        },
+        error: function (xhr, ajaxOptions, thrownError) {
+            console.log('Error: ' + xhr.status + ' ' + thrownError);
+            $('#nasaTitle').html('keine NASA-Daten gefunden');
+        }
+    });
+}
