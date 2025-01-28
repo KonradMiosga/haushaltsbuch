0dfe64c01142727cf074e18f0ed405ba4d8dcb28
Michael <...>
2025-01-28 17:24:00 +0100

    change in config, adresse backend

--- public/js/config.js
+++ public/js/config.js
@@ -1,7 +1,7 @@
 const config = {
 
-    apiUrl: 'http://localhost:8080/api',
-    //apiUrl: 'https://calm-harbor-02759-2a1f6d8dfef7.herokuapp.com/api',
+    //apiUrl: 'http://localhost:8080/api',
+    apiUrl: 'https://calm-harbor-02759-2a1f6d8dfef7.herokuapp.com/api',
 
     // graphqlUrl: 'http://localhost:8080/graphql'
     graphqlUrl: 'https://guarded-anchorage-34944-dcf8e00a5c64.herokuapp.com/graphql',
